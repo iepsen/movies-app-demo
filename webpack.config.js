9d17a4025e2bef40c7b619cf156b3def511f5c8f
Marcelo Iepsen <iepsen@gmail.com>
2018-10-10 21:17:08 -0300

    added config files

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new HtmlWebPackPlugin({
+      template: 'public/index.html'
+  })],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+};
