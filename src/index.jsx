ec8d2af4f75a766d87e588d9a168faf6b364d6dc
Marcelo Iepsen <iepsen@gmail.com>
2018-10-12 14:19:58 -0300

    passing the history object to routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { HomeRouteComponent, PlayerRouteComponent } from './routes'
+import { BrowserRouter, Switch, Route, Router } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
+import { HomeRouteComponent, MovieRouteComponent } from './routes'
 import './routes/styles/global.scss'
 
 class Application extends React.Component {
+    constructor(props) {
+        super(props)
+        this.history = createBrowserHistory()
+    }
     render() {
         return (
             <BrowserRouter>
-                <Switch>
-                    <Route exact path="/" component={HomeRouteComponent} />
-                    <Route exact path="/player/:id" component={PlayerRouteComponent} />
-                </Switch>
+                <Router history={this.history}>
+                    <Switch>
+                        <Route exact path="/" component={HomeRouteComponent} />
+                        <Route exact path="/movie/:id" component={MovieRouteComponent} />
+                    </Switch>
+                </Router>
             </BrowserRouter>
         )
     }
