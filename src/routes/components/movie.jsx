fa84d20c4da048d6bdec8e260b38df4b25ec4b52
Marcelo Iepsen <iepsen@gmail.com>
2018-10-12 21:11:32 -0300

    added back support

--- src/routes/components/movie.jsx
+++ src/routes/components/movie.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import { VideoPlayerComponent } from '../../presentation/components/video-player'
+import { MovieInteractor } from '../../domain/movie-interactor'
 
 export class MovieRouteComponent extends React.Component {
+    constructor(props) {
+        super(props)
+        this.onTimeUpdate = this.onTimeUpdate.bind(this)
+        this.onBack = this.onBack.bind(this)
+        this.movie = this.props.location.state.movie
+        this.interactor = new MovieInteractor()
+    }
+
+    onBack() {
+        this.props.history.goBack()
+    }
+
+    onTimeUpdate(watched) {
+        this.interactor.setStoreProgress(this.movie.id, watched)
+    }
+
     render() {
         return (
-            <VideoPlayerComponent data={this.props.location.state.movie} />
+            <VideoPlayerComponent onBack={this.onBack} onTimeUpdate={this.onTimeUpdate} data={this.movie} />
         )
     }
 }
