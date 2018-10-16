0546584eb0e21cb93b078cd4725e21fd56c7b12d
Marcelo Iepsen <iepsen@gmail.com>
2018-10-15 21:13:53 -0300

    jsdoc on service layer

--- src/data/movies-service.js
+++ src/data/movies-service.js
@@ -1,10 +1,19 @@
 import { MovieEntity } from '../domain/movie-entity'
 
+/**
+ * Movies Service
+ */
 export class MoviesService {
     constructor() {
         this.serviceUrl = 'https://2qcg0zv57a.execute-api.us-west-2.amazonaws.com/default/fetchVideos'
     }
 
+    /**
+     * Fetch movies
+     * @async
+     * @param {string} url - The service url.
+     * @returns {Promise<Array>} The movie entries.
+     */
     async get() {
         return fetch(this.serviceUrl)
             .then(response => response.json())
@@ -16,10 +25,20 @@ export class MoviesService {
             }))
     }
 
+    /**
+     * Save the movie progress
+     * @param {string} key - The movie key to store.
+     * @param {string} value - The rogress value.
+     */
     setStoredProgress(key, value) {
         localStorage.setItem(key, value)
     }
 
+    /**
+     * Get the movie progress
+     * @param {string} key - The movie key.
+     * @returns {string} The movie progress.
+     */
     getStoredProgress(key) {
         return localStorage.getItem(key)
     }
