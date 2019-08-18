feba90e856cd9f9480cc0863f7df0cb49d87e888
Marcelo Iepsen <iepsen@gmail.com>
2019-08-18 12:15:57 +0200

    Added content type and acls

--- deploy.js
+++ deploy.js
@@ -1,22 +1,25 @@
 const fs = require('fs')
 const S3 = require('aws-sdk/clients/s3')
 
-const files = ['index.html', 'main.js']
+const files = [
+    { name: 'index.html', contentType: 'text/html' }, 
+    { name: 'main.js', contentType: 'text/javascript' }
+]
 const s3 = new S3({
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
-const options = { partSize: 10 * 1024 * 1024, queueSize: 1 }
 
 const upload = () => {
     return files.map(file => {
-        const content = fs.readFileSync(`dist/${file}`)
         const params = {
+            ACL: 'public-read',
             Bucket: 'movies-app-demo',
-            Key: file,
-            Body: JSON.stringify(content)
+            Key: file.name,
+            Body: fs.readFileSync(`dist/${file.name}`),
+            ContentType: file.contentType
         }
-        s3.upload(params, options, err => !err)
+        s3.upload(params, null, err => !err)
     })
 }
 upload()
