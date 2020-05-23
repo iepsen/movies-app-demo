abd8c557b5febd46d661fa31c80dc9efb8f66976
Marcelo Iepsen <iepsen@gmail.com>
2020-05-23 20:43:28 +0200

    Convert Movie to functional

--- src/routes/components/movie.jsx
+++ src/routes/components/movie.jsx
@@ -1,35 +1,28 @@
 /** @module presentation/routes */
 import React from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
 import { VideoPlayerComponent } from '../../presentation/components/video-player'
 import { MovieInteractor } from '../../domain/movie-interactor'
 
 /**
  * MovieRouteComponent
  */
-export class MovieRouteComponent extends React.Component {
-    /**
-     * Initialize the MovieRouteComponent 
-     * @param {React.Props} props - The component props
-     */
-    constructor(props) {
-        super(props)
-        this.onTimeUpdate = this.onTimeUpdate.bind(this)
-        this.onBack = this.onBack.bind(this)
-        this.movie = this.props.location.state.movie
-        this.interactor = new MovieInteractor()
-    }
-
+const MovieRouteComponent = () => {
+    const interactor = new MovieInteractor()
+    const history = useHistory()
+    const location = useLocation()
+    const { movie } = location.state
+    
     /**
      * Dispatched on user returns from the VideoPlayerComponent
      * and store the progress time.
      * @param {string} watched - Seconds watched
      */
-    onBack(watched) {
+    const onBack = watched => {
         if (watched) {
-            this.interactor.setStoredProgress(this.movie.id, 0)
+            interactor.setStoredProgress(movie.id, '0')
         }
-
-        this.props.history.goBack()
+        history.goBack()
     }
 
     /**
@@ -37,13 +30,13 @@ export class MovieRouteComponent extends React.Component {
      * and store the progress time.
      * @param {string} watched - Seconds watched
      */
-    onTimeUpdate(watched) {
-        this.interactor.setStoredProgress(this.movie.id, watched)
+    const onTimeUpdate = watched => {
+        interactor.setStoredProgress(movie.id, watched)
     }
 
-    render() {
-        return (
-            <VideoPlayerComponent onBack={this.onBack} onTimeUpdate={this.onTimeUpdate} data={this.movie} />
-        )
-    }
+    return (
+        <VideoPlayerComponent onBack={onBack} onTimeUpdate={onTimeUpdate} data={movie} />
+    )
 }
+
+export { MovieRouteComponent }
