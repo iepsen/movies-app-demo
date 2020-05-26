51fdda8eaa4b3b7b0acd2213eca0dc1bee1f876e
Marcelo Iepsen <iepsen@gmail.com>
2020-05-26 13:45:52 +0200

    Return model instances instead of API response

--- src/services/trending.ts
+++ src/services/trending.ts
@@ -1,15 +1,25 @@
 import { API_ENDPOINT, API_KEY } from './constants'
 import {
-  TrendingMoviesResponse, TrendingShowsResponse
+  TrendingResponse, TrendingMoviesResponse, TrendingShowsResponse
 } from './interfaces/TrendingResponse'
+import { MovieModel } from '../models/interfaces/MovieModel'
+import { Movie } from '../models/Movie'
+import { ShowModel } from '../models/interfaces/ShowModel'
+import { Show } from '../models/Show'
 
-const request = (path: string): Promise<any> => {
+const request = async (path: string): Promise<TrendingResponse> => {
   const url = `${API_ENDPOINT}${path}?api_key=${API_KEY}`
   return fetch(url).then((response: Response) => response.json())
 }
 
-const getTrendingMovies = (): Promise<TrendingMoviesResponse> => request('/trending/movie/day')
+const getTrendingMovies = async (): Promise<MovieModel[]> => {
+  const movies = await request('/trending/movie/day') as TrendingMoviesResponse
+  return movies.results.map(movie => Movie(movie))
+}
 
-const getTrendingShows = (): Promise<TrendingShowsResponse> => request('/trending/tv/day')
+const getTrendingShows = async (): Promise<ShowModel[]> => {
+  const shows = await request('/trending/tv/day') as TrendingShowsResponse
+  return shows.results.map(show => Show(show))
+}
 
 export { getTrendingMovies, getTrendingShows }
