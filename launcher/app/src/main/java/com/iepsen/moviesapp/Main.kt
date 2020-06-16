26ac40dee6cc804289467af3b12cd55ad30992ed
Marcelo Iepsen <iepsen@gmail.com>
2020-06-16 15:21:47 +0200

    Add logic to launch YouTube app when user choose to play the trailer

--- launcher/app/src/main/java/com/iepsen/moviesapp/Main.kt
+++ launcher/app/src/main/java/com/iepsen/moviesapp/Main.kt
@@ -1,19 +1,61 @@
 package com.iepsen.moviesapp
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Window
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
+import com.google.android.youtube.player.YouTubeIntents
 
 class MainActivity : Activity() {
+    private var webView: WebView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
-        val webView: WebView = findViewById(R.id.webview)
-        webView.settings.javaScriptEnabled = true
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl(BuildConfig.WEBVIEW_URL)
+        webView = findViewById(R.id.webview)
+        webView?.settings?.javaScriptEnabled = true
+        webView?.webViewClient = object : WebViewClient() {
+            override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
+                if (url === null) return
+
+                val pattern = Regex("https?:\\/\\/[a-z0-9.:]+\\/#\\/video\\/([0-9a-zA-Z]+)")
+                val match = pattern.find(url)
+                if (match === null) return
+
+                val (videoId) = match.destructured
+
+                createIntent(videoId)
+
+                super.doUpdateVisitedHistory(view, url, isReload)
+            }
+        }
+        webView?.webChromeClient = WebChromeClient()
+        webView?.loadUrl(BuildConfig.WEBVIEW_URL)
+    }
+
+    fun createIntent(videoId: String) {
+        if (YouTubeIntents.canResolvePlayVideoIntent(this)) {
+            startActivity(YouTubeIntents.createPlayVideoIntent(this, videoId))
+        } else {
+           val webIntent = Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.youtube.com/watch?v=$videoId"));
+
+            startActivity(webIntent);
+        }
+        goBack()
+    }
+
+    private fun goBack() {
+        when {
+            webView?.canGoBack()!! -> webView!!.goBack()
+            else -> super.onBackPressed()
+        }
+    }
+
+    override fun onBackPressed() {
+        goBack()
     }
 }
