b9cb2c2f885625075f8456a8bbffd352d4e357da
Marcelo Iepsen <iepsen@gmail.com>
2021-12-25 12:03:37 +0100

    Replace webpack with Vite

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter, Route } from 'react-router-dom'
+import { Home, Movie, Video, Show } from './views'
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/movie/:id" component={Movie} />
+      <Route exact path="/show/:id" component={Show} />
+      <Route exact path="/video/:id" component={Video} />
+    </HashRouter>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- tools/deploy.js
+++ tools/deploy.js
@@ -3,7 +3,8 @@ const S3 = require('aws-sdk/clients/s3')
 
 const files = [
   { name: 'index.html', contentType: 'text/html' },
-  { name: 'main.js', contentType: 'text/javascript' }
+  { name: 'assets/index.js', contentType: 'text/javascript' },
+  { name: 'assets/vendor.js', contentType: 'text/javascript' }
 ]
 const s3 = new S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -11,7 +12,7 @@ const s3 = new S3({
 })
 
 const upload = () => {
-  return files.map((file) => {
+  return files.map(file => {
     const params = {
       ACL: 'public-read',
       Bucket: 'movies-app-demo',
@@ -19,7 +20,7 @@ const upload = () => {
       Body: fs.readFileSync(`dist/${file.name}`),
       ContentType: file.contentType
     }
-    s3.upload(params, null, (err) => !err)
+    s3.upload(params, null, err => !err)
   })
 }
 upload()
